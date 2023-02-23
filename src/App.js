6badf48272a9561dfb35175ca0f8bcf60dea9167
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-23 11:41:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fit,f_auto,g_center,pg_1,q_60,w_965/646b91cfec3270cc1f6f8982140bdeb3.jpg" />
+        </div>
+        <div className="text">
+          <h2>Elden Ring Sells 20 Million Copies, Makes Excellent Point</h2>
+          <p>
+            It was announced earlier today that Elden Ring, FromSoftware’s 2022
+            Game Of The Year For Loads Of People (including most staff at this
+            website), has sold 20 million copies worldwide since its release a
+            year ago.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fit,f_auto,g_center,pg_1,q_60,w_965/646b91cfec3270cc1f6f8982140bdeb3.jpg" />
+        </div>
+        <div className="text">
+          <h2>Elden Ring Sells 20 Million Copies, Makes Excellent Point</h2>
+          <p>
+            It was announced earlier today that Elden Ring, FromSoftware’s 2022
+            Game Of The Year For Loads Of People (including most staff at this
+            website), has sold 20 million copies worldwide since its release a
+            year ago.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fit,f_auto,g_center,pg_1,q_60,w_965/646b91cfec3270cc1f6f8982140bdeb3.jpg" />
+        </div>
+        <div className="text">
+          <h2>Elden Ring Sells 20 Million Copies, Makes Excellent Point</h2>
+          <p>
+            It was announced earlier today that Elden Ring, FromSoftware’s 2022
+            Game Of The Year For Loads Of People (including most staff at this
+            website), has sold 20 million copies worldwide since its release a
+            year ago.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
