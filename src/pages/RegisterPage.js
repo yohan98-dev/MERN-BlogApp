272b050a0d58123780aa96e4b0f4749afdf67f8b
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-23 21:42:44 +0530

    Login page and registration connected with mongo DB

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -6,11 +6,17 @@ export default function RegisterPage() {
 
   async function register(ev) {
     ev.preventDefault();
-    await fetch("http://localhost:4000/register", {
+
+    const response = await fetch("http://localhost:4000/register", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "content-Type": "application/json" },
     });
+    if (response.status === 200) {
+      alert("registration Successful");
+    } else {
+      alert("Registraion failed");
+    }
   }
 
   return (
