c120a85904fa77c267b7a92b17583f7ea1ba420b
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-23 13:17:39 +0530

    Login page and register page layout added

--- src/App.js
+++ src/App.js
@@ -1,60 +1,20 @@
 import "./App.css";
-
+import Post from "./Post";
+import Header from "./Header";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/indexPage";
+import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">
-          MyBlog
-        </a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fit,f_auto,g_center,pg_1,q_60,w_965/646b91cfec3270cc1f6f8982140bdeb3.jpg" />
-        </div>
-        <div className="text">
-          <h2>Elden Ring Sells 20 Million Copies, Makes Excellent Point</h2>
-          <p>
-            It was announced earlier today that Elden Ring, FromSoftware’s 2022
-            Game Of The Year For Loads Of People (including most staff at this
-            website), has sold 20 million copies worldwide since its release a
-            year ago.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fit,f_auto,g_center,pg_1,q_60,w_965/646b91cfec3270cc1f6f8982140bdeb3.jpg" />
-        </div>
-        <div className="text">
-          <h2>Elden Ring Sells 20 Million Copies, Makes Excellent Point</h2>
-          <p>
-            It was announced earlier today that Elden Ring, FromSoftware’s 2022
-            Game Of The Year For Loads Of People (including most staff at this
-            website), has sold 20 million copies worldwide since its release a
-            year ago.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fit,f_auto,g_center,pg_1,q_60,w_965/646b91cfec3270cc1f6f8982140bdeb3.jpg" />
-        </div>
-        <div className="text">
-          <h2>Elden Ring Sells 20 Million Copies, Makes Excellent Point</h2>
-          <p>
-            It was announced earlier today that Elden Ring, FromSoftware’s 2022
-            Game Of The Year For Loads Of People (including most staff at this
-            website), has sold 20 million copies worldwide since its release a
-            year ago.
-          </p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/Post.js
@@ -0,0 +1,22 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fit,f_auto,g_center,pg_1,q_60,w_965/646b91cfec3270cc1f6f8982140bdeb3.jpg" />
+      </div>
+      <div className="text">
+        <h2>Elden Ring Sells 20 Million Copies, Makes Excellent Point</h2>
+        <p className="info">
+          <a className="author">Yohan hettiarachchi</a>
+          <time>2023-02-23 11:48</time>
+        </p>
+        <p className="summary">
+          It was announced earlier today that Elden Ring, FromSoftware’s 2022
+          Game Of The Year For Loads Of People (including most staff at this
+          website), has sold 20 million copies worldwide since its release a
+          year ago.
+        </p>
+      </div>
+    </div>
+  );
+}
