5cc80a6704c53cc09d289a09ca42f21fc503d0a9
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-23 13:45:31 +0530

    Login page and register page styles added

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,6 @@
 export default function RegisterPage() {
   return (
-    <form action="">
+    <form action="" className="register">
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
       <button>Register</button>
--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,6 +1,6 @@
 export default function LoginPage() {
   return (
-    <form action="">
+    <form className="login">
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
       <button>Login</button>
