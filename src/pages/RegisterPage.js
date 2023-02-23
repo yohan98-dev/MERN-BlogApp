7807c103215aafb5bacac25d640afe0557e888c6
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-23 17:17:44 +0530

    api connection created

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "content-Type": "application/json" },
+    });
+  }
+
   return (
-    <form action="" className="register">
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+    <form action="" className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUserName(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
       <button>Register</button>
     </form>
   );
--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,6 +1,7 @@
 export default function LoginPage() {
   return (
     <form className="login">
+      <h1>Login</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
       <button>Login</button>
